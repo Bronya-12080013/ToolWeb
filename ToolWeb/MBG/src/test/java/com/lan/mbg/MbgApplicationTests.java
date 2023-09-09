deb75b9db0d581bb7669ffad5717135acfaf1653
Bronya-12080013 <84848688+Bronya-12080013@users.noreply.github.com>
2023-09-09 13:36:56 +0800

    Add files via upload

---
+++ ToolWeb/MBG/src/test/java/com/lan/mbg/MbgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lan.mbg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
